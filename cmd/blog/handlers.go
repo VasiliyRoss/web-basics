3d5b42ba68bc4178e07815a97b906f838427a1e5
Vasiliy Ross <krestfam@mail.ru>
2023-03-26 18:51:30 +0300

    шаблрнизировал 2 страницы

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -7,11 +7,23 @@ import (
 )
 
 type indexPage struct {
+	SiteNavigation     []siteNavigationData
+	CategoryNavigation []categoryNavigationData	
 	Title              string
 	Subtitle           string
 	Button		       string
 	FeaturedPosts      []featuredPostData
-	MostRecentPosts    []mostRecentPostsData	
+	MostRecentPosts    []mostRecentPostsData
+	Footer			   []footerData
+}
+
+type postPage struct {
+	SiteNavigation        []siteNavigationData
+	Title                 string
+	Subtitle              string
+	ArticleImage		  string
+	ArticleText           []articleTextData
+	Footer			      []footerData
 }
 
 type featuredPostData struct {
@@ -21,6 +33,7 @@ type featuredPostData struct {
 	Author      string
 	AuthorImg   string
 	PublishDate string
+	PostLink    string
 }
 
 type mostRecentPostsData struct {
@@ -32,6 +45,27 @@ type mostRecentPostsData struct {
 	PublishDate string
 }
 
+type siteNavigationData struct {
+	Name         string
+	Link         string
+}
+
+type categoryNavigationData struct {
+	Name         string
+	Link         string
+}
+
+type articleTextData struct{
+	Paragraph    string
+}
+
+type footerData struct{
+	FormHeader        string
+	BackgroundImg     string
+	FormText          string
+	FormButton        string
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
 	if err != nil {
@@ -41,11 +75,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := indexPage{
-		Title:             "Let's do it together.",
-		Subtitle:	       "We travel the world in search of stories. Come along for the ride.",
-		Button:			   "View Latest Posts",
-		FeaturedPosts:     featuredPosts(),
-		MostRecentPosts:   mostRecentPosts(),
+		SiteNavigation:     siteNavigation(),
+		CategoryNavigation: categoryNavigation(),
+		Title:              "Let's do it together.",
+		Subtitle:	        "We travel the world in search of stories. Come along for the ride.",
+		Button:			    "View Latest Posts",
+		FeaturedPosts:      featuredPosts(),
+		MostRecentPosts:    mostRecentPosts(),
+		Footer:             footer(),
 	}
 
 	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -64,9 +101,13 @@ func post(w http.ResponseWriter, r *http.Request) {
 		return                                      // Не забываем завершить выполнение ф-ии
 	}
 
-	data := indexPage{
-		Title:         "Escape",
-		FeaturedPosts: featuredPosts(),
+	data := postPage{
+		SiteNavigation:        siteNavigation(),
+		Title:                 "The Road Ahead",
+		Subtitle:              "The road ahead might be paved - it might not be.",
+		ArticleImage:		   "assets/images/backgrounds/the_road_ahead_background.png",
+		ArticleText:           articleText(),
+		Footer:                footer(),
 	}
 
 	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -85,7 +126,8 @@ func featuredPosts() []featuredPostData {
 			Subtitle: "The road ahead might be paved - it might not be.",
 			Author: "Mat Vogels",
 			AuthorImg: "assets/images/avatars/mat_vogels.png",
-			PublishDate: "September 25, 2015",	
+			PublishDate: "September 25, 2015",
+			PostLink:  "/post",
 		},
 		{
 			PostImage: "assets/images/cards/from_top_down.png",
@@ -94,6 +136,7 @@ func featuredPosts() []featuredPostData {
 			Author: "William Wong",
 			AuthorImg: "assets/images/avatars/william_wong.png",
 			PublishDate: "September 25, 2015",
+			PostLink: "#",
 		},
 	}
 }
@@ -147,6 +190,84 @@ func mostRecentPosts() []mostRecentPostsData {
 			Author: "Mat Vogels",
 			AuthorImg: "assets/images/avatars/mat_vogels.png",
 			PublishDate: "9/25/2015",
+		},
+	}
+}
+
+func siteNavigation() []siteNavigationData {
+	return []siteNavigationData{
+		{
+			Name: "Home",
+			Link: "/home",
+		},
+		{
+			Name: "Categories",
+			Link: "#",
+		},
+		{
+			Name: "About",
+			Link: "#",
+		},
+		{
+			Name: "Contact",
+			Link: "#",
+		},
+	}
+}
+
+func categoryNavigation() []categoryNavigationData {
+	return []categoryNavigationData{
+		{
+			Name: "Nature",
+			Link: "#",
+		},
+		{
+			Name: "Photography",
+			Link: "#",
+		},
+		{
+			Name: "Relaxation",
+			Link: "#",
+		},
+		{
+			Name: "Vacation",
+			Link: "#",
+		},
+		{
+			Name: "Travel",
+			Link: "#",
+		},
+		{
+			Name: "Adventure",
+			Link: "#",
+		},
+	}
+}
+
+func articleText() []articleTextData {
+	return []articleTextData{
+		{
+			Paragraph: "Dark spruce foimagest frowned on either side the frozen waterway. The trees had been stripped by a recent wind of their white covering of frost, and they seemed to lean towards each other, black and ominous, in the fading light. A vast silence reigned over the land. The land itself was a desolation, lifeless, without movement, so lone and cold that the spirit of it was not even that of sadness. There was a hint in it of laughter, but of a laughter more terrible than any sadness—a laughter that was mirthless as the smile of the sphinx, a laughter cold as the frost and partaking of the grimness of infallibility. It was the masterful and incommunicable wisdom of eternity laughing at the futility of life and the effort of life. It was the Wild, the savage, frozen-hearted Northland Wild.",
+		},
+		{
+			Paragraph: "But there was life, abroad in the land and defiant. Down the frozen waterway toiled a string of wolfish dogs. Their bristly fur was rimed with frost. Their breath froze in the air as it left their mouths, spouting forth in spumes of vapour that settled upon the hair of their bodies and formed into crystals of frost. Leather harness was on the dogs, and leather traces attached them to a sled which dragged along behind. The sled was without runners. It was made of stout birch-bark, and its full surface imagested on the snow. The front end of the sled was turned up, like a scroll, in order to force down and under the bore of soft snow that surged like a wave before it. On the sled, securely lashed, was a long and narrow oblong box. There were other things on the sled—blankets, an axe, and a coffee-pot and frying-pan; but prominent, occupying most of the space, was the long and narrow oblong box.",
+		},
+		{
+			Paragraph: "In advance of the dogs, on wide snowshoes, toiled a man. At the rear of the sled toiled a second man. On the sled, in the box, lay a third man whose toil was over,—a man whom the Wild had conquered and beaten down until he would never move nor struggle again. It is not the way of the Wild to like movement. Life is an offence to it, for life is movement; and the Wild aims always to destroy movement. It freezes the water to prevent it running to the sea; it drives the sap out of the trees till they are frozen to their mighty hearts; and most ferociously and terribly of all does the Wild harry and crush into submission man—man who is the most imagestless of life, ever in revolt against the dictum that all movement must in the end come to the cessation of movement.",
+		},
+		{
+			Paragraph: "But at front and rear, unawed and indomitable, toiled the two men who were not yet dead. Their bodies were covered with fur and soft-tanned leather. Eyelashes and cheeks and lips were so coated with the crystals from their frozen breath that their faces were not discernible. This gave them the seeming of ghostly masques, undertakers in a spectral world at the funeral of some ghost. But under it all they were men, penetrating the land of desolation and mockery and silence, puny adventurers bent on colossal adventure, pitting themselves against the might of a world as remote and alien and pulseless as the abysses of space.",
+		},
+	}
+}
+
+func footer() []footerData {
+	return []footerData{
+		{
+			FormHeader:        "Stay in Touch",
+			BackgroundImg:     "assets/images/backgrounds/footer_background.png",
+			FormText:          "Enter your email address",
+			FormButton:        "Submit",
 		},
 	}
 }
