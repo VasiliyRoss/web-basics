a7a0d4e470a8a8eefd546a5070627deb6527e465
Basil Ross <91021731+Basil-Ross@users.noreply.github.com>
2023-03-30 22:31:40 +0300

    добавил запросы в базу

---
+++ data/sql/posts.sql
@@ -0,0 +1,11 @@
+INSERT INTO post (title, subtitle, author, author_url, publish_date, image_url, featured) 
+VALUES 
+ ('The Road Ahead', 'The road ahead might be paved - it might not be.', 'Mat Vogels', 'assets/images/avatars/mat_vogels.png', '9/25/2015', 'assets/images/cards/the_road_ahead.png', 1),
+ ('From Top Down', 'Once a year, go someplace you’ve never been before.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/from_top_down.png', 1),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0),
+ ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'William Wong', 'assets/images/avatars/william_wong.png', '9/25/2015', 'assets/images/cards/still_standing_tall.jpg', 0)
+;
