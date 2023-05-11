d4d698243ea21c193c5505718160d3f05821b660
Basil Ross <91021731+Basil-Ross@users.noreply.github.com>
2023-05-12 01:42:35 +0300

    попытка реализовать загрузку картинок

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,19 +1,41 @@
 const form = document.getElementById('form')
-const formData = new FormData(form)
-const title = formData.get('post_title')
-const subtitle = formData.get('post_short_description')
-const author = formData.get('post_author_name')
-const authorPhoto = formData.get('post_author_photo')
-const publishDate = formData.get('post_publish_date')
-const postImage = formData.get('post_image')
-const cardImage = formData.get('post_card_image')
-const content = formData.get('content')
+var authorPhoto
+var cardImage
+var postImage
 
+function loadAndPreviewAuthorPhoto() {
+  const placeholder = document.getElementById("authorPhotoPlaceholder")
+  const preview = document.getElementById("authorPhotoDemo")
+  const file = document.getElementById("authorPhoto").files[0]
+  const reader = new FileReader()
 
+  reader.addEventListener(
+    "load",
+    () => {
+      preview.src = reader.result
+      authorPhoto = reader.result
+      placeholder.classList.toggle("post-description__author-photo_hidden")
+      preview.classList.toggle("post-description__author-photo_hidden")
+    },
+    false
+  )
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+
+}
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  const formData = new FormData(form)
+  const title = formData.get('post_title')
+  const subtitle = formData.get('post_short_description')
+  const author = formData.get('post_author_name')
+  const publishDate = formData.get('post_publish_date')
+  const content = formData.get('content')
 
   let post = JSON.stringify({
     title: title,
