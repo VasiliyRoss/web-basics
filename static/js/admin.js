f7b49cb8c6bc549788749cc94a33f36ff39dc6dc
Vasiliy Ross <krestfam@mail.ru>
2023-06-13 16:26:05 +0300

    добавил анимации на странице админки

--- static/js/admin.js
+++ static/js/admin.js
@@ -7,19 +7,67 @@ function displayText(inputId, displayClass) {
   }
 }
 
+/*document.getElementById('toggleButton').addEventListener('click', function() {
+  var block = document.getElementById('alertSuccess');
+  if (block.classList.contains('show')) {
+    block.classList.remove('show');
+    block.classList.add('hide');
+  } else {
+    block.classList.remove('hide');
+    block.classList.add('show');
+  }
+});*/
+
+function resetFileInput(id, buttonId) {
+  document.getElementById(id).value = null;
+  document.getElementById('placeholder-' + id).style.backgroundImage = 'url(/static/images/decorations/placeholder-' + id + '.png)';
+  document.getElementById('preview-' + id).style.backgroundImage = 'url(/static/images/decorations/preview-' + id + '.png)';
+  document.getElementById('inputImage-' + id).value = null;
+  document.getElementById(buttonId).classList.add('block_hidden');
+  
+  var subdescription = document.getElementById('subdescription-' + id);
+  if (subdescription != null) {
+    subdescription.classList.remove('block_hidden')
+  }
+
+  var upload = document.getElementById('upload-' + id)
+  if (upload != null) {
+    upload.classList.remove('block_hidden')
+  }
+
+  var uploadNew = document.getElementById('uploadNew-' + id)
+  uploadNew.classList.add('block_hidden')
+}
+
 function uploadImage(event, id) {
   var input = event.target;
   var reader = new FileReader();
 
   reader.onload = function () {
     var placeholderImage = document.getElementById('placeholder-' + id);
-    placeholderImage.src = reader.result;
+    placeholderImage.style.backgroundImage = 'url(' + reader.result + ')';
 
     var previewImage = document.getElementById('preview-' + id);
-    previewImage.src = reader.result;
+    previewImage.style.backgroundImage = 'url(' + reader.result + ')';
 
     var inputImage = document.getElementById('inputImage-' + id);
     inputImage.value = reader.result;
+
+    var removeButton = document.getElementById('removeButton-' + id);
+    removeButton.classList.remove('block_hidden')
+
+    var subdescription = document.getElementById('subdescription-' + id);
+    if (subdescription != null) {
+      subdescription.classList.add('block_hidden')
+    }
+
+    var upload = document.getElementById('upload-' + id)
+    if (upload != null) {
+      upload.classList.add('block_hidden')
+    }
+
+    var uploadNew = document.getElementById('uploadNew-' + id)
+    uploadNew.classList.remove('block_hidden')
   };
 
   reader.readAsDataURL(input.files[0]);
@@ -45,7 +93,6 @@ form.addEventListener('submit', async (e) => {
   }
 
   for (var pair of formData.entries()) {
-    console.log(pair[0]);
     if (pair[0] === "publish_date") {
       formValues[pair[0]] = convertDateFormat(pair[1]);
     } else {
